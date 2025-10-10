8b6f2c8d8ea0da48ca54a25054f98085aa3d004e
Denky02 <rizkyramadhan05214@gmail.com>
2025-10-10 12:03:05 +0800

    Update PRAK110_2510817210004_MuhammadRizkyRamadhani.c

--- PRAK110_2510817210004_MuhammadRizkyRamadhani.c
+++ PRAK110_2510817210004_MuhammadRizkyRamadhani.c
@@ -2,20 +2,20 @@
 #include <math.h>
 
 int main() {
-    int a = 5;
-    int b = 12;
-    float c = sqrt(a * a + b * b);
-    float keliling_segitiga = a + b + c;
-    float luas_segitiga = 0.5 * a * b;
+    int alas = 5;
+    int tinggi = 12;
+    double miring = sqrt(pow(alas, 2) + pow(tinggi, 2));
+    double keliling_segitiga = alas + tinggi + miring;
+    double luas_segitiga = 0.5 * alas * tinggi;
 
     printf("Diketahui :\n");
-    printf("Alas = %d cm\n", a);
-    printf("Tinggi = %d cm\n\n", b);
+    printf("Alas = %d cm\n", alas);
+    printf("Tinggi = %d cm\n\n", tinggi);
 
     printf("Jawab :\n");
-    printf("Sisi A = %d cm\n", a);
-    printf("Sisi B = %d cm\n", b);
-    printf("Sisi C = %.0f cm\n", c);
+    printf("Sisi A = %d cm\n", tinggi);
+    printf("Sisi B = %.0f cm\n", miring);
+    printf("Sisi C = %d cm\n", alas);
     printf("Keliling = %.0f cm\n", keliling_segitiga);
     printf("Luas = %.0f cm\n", luas_segitiga);
 
