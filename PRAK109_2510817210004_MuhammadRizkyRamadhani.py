3a05c7d2eb8275bb8a761e34ac14ea557f73301a
Denky02 <rizkyramadhan05214@gmail.com>
2025-10-10 12:04:05 +0800

    Update PRAK109_2510817210004_MuhammadRizkyRamadhani.py

--- PRAK109_2510817210004_MuhammadRizkyRamadhani.py
+++ PRAK109_2510817210004_MuhammadRizkyRamadhani.py
@@ -1,5 +1,5 @@
 jumlah_pasukan_yz = 958730
-jumlah_pahlawan_altar = 5
+jumlah_pahlawan_altar = 5 #(Zilong, Ling, Baxia, Wanwan, Chang’e)
 jumlah_pasukan_yang_harus_dikalahkan = jumlah_pasukan_yz/jumlah_pahlawan_altar
 
 print(f'Jumlah pasukan yang dibawa Yu Zhong = {jumlah_pasukan_yz}\n\nJumlah pahlawan = {jumlah_pahlawan_altar}\n\nJumlah pasukan yang harus dikalahkan setiap pahlawan adalah {jumlah_pasukan_yang_harus_dikalahkan:.0f} pasukan ')
